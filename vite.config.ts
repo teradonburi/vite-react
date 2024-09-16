eb656f1602ea7ec5dc91bd8cf96d781df0af8b4f
teradonburi <daikiterai@gmail.com>
2024-09-16 19:20:01 +0900

    pnpm

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,22 @@
-import { resolve } from 'path'
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin'
+import { resolve } from "node:path";
+import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
+import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  root: 'src',
-  plugins: [react(), vanillaExtractPlugin()],
-  build: {
-    outDir: resolve(__dirname, 'dist'),
-    emptyOutDir: true,
-    rollupOptions: {
-      input: {
-        '': resolve(__dirname, 'src/index.html'),
-      },
-      output: {
-        entryFileNames: `assets/[name]/bundle.js`,
-      },
-    },
-  },
-})
+	root: "src",
+	plugins: [react(), vanillaExtractPlugin()],
+	build: {
+		outDir: resolve(__dirname, "dist"),
+		emptyOutDir: true,
+		rollupOptions: {
+			input: {
+				"": resolve(__dirname, "src/index.html"),
+			},
+			output: {
+				entryFileNames: "assets/[name]/bundle.js",
+			},
+		},
+	},
+});
