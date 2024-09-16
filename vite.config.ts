91a52047409b12b94d6714a39ff6aa78f3b6face
teradonburi <daikiterai@gmail.com>
2024-09-16 20:03:55 +0900

    vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,26 @@
 import { resolve } from "node:path";
-import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
-import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+	base: "./",
 	root: "src",
-	plugins: [react(), vanillaExtractPlugin()],
+	plugins: [],
+	publicDir: resolve(__dirname, "public"),
 	build: {
+		// distフォルダに出力
 		outDir: resolve(__dirname, "dist"),
+		// 存在しないときはフォルダを作成する
 		emptyOutDir: true,
+		copyPublicDir: true,
 		rollupOptions: {
+			// entry pointがあるindex.htmlのパス
 			input: {
 				"": resolve(__dirname, "src/index.html"),
 			},
+			// bundle.jsを差し替えする
 			output: {
-				entryFileNames: "assets/[name]/bundle.js",
+				entryFileNames: "assets/bundle.js",
 			},
 		},
 	},
