89c9389182bf6abee8e9a6907d56516c40558837
teradonburi <daikiterai@gmail.com>
2024-09-16 19:19:20 +0900

    pnpm

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,22 @@
-import React from 'react'
-import { classes } from './app.css'
+import React from "react";
+import { classes } from "./app.css";
 
 function App() {
-  const [count, setCount] = React.useState(0)
+	const [count, setCount] = React.useState(0);
 
-  return (
-    <div className={classes.root}>
-      <h1 className={classes.h1}>Vite + React</h1>
-      <div>
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-    </div>
-  )
+	return (
+		<div className={classes.root}>
+			<h1 className={classes.h1}>Vite + React</h1>
+			<div>
+				<button type="button" onClick={() => setCount((count) => count + 1)}>
+					count is {count}
+				</button>
+				<p>
+					Edit <code>src/App.tsx</code> and save to test HMR
+				</p>
+			</div>
+		</div>
+	);
 }
 
-export default App
+export default App;
